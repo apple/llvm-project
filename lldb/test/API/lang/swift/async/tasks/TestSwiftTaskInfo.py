b0ecfe41bc706392071f7f7794649541768777a3
Dave Lee <davelee.com@gmail.com>
2025-01-27 16:18:30 -0800

    Switch order of task commands

--- lldb/test/API/lang/swift/async/tasks/TestSwiftTaskInfo.py
+++ lldb/test/API/lang/swift/async/tasks/TestSwiftTaskInfo.py
@@ -8,7 +8,8 @@ import re
 
 def _tail(output):
     """Delete the first line of output text."""
-    return re.subn(r"^.*\n", "", output, count=1)
+    result, _ = re.subn(r"^.*\n", "", output, count=1)
+    return result
 
 
 class TestCase(TestBase):
@@ -17,8 +18,8 @@ class TestCase(TestBase):
         lldbutil.run_to_source_breakpoint(
             self, "break here", lldb.SBFileSpec("main.swift")
         )
-        self.runCmd("language swift task info")
-        task_info_output = self.res.GetOutput()
         self.runCmd("frame variable task")
         frame_variable_output = self.res.GetOutput()
+        self.runCmd("language swift task info")
+        task_info_output = self.res.GetOutput()
         self.assertEqual(_tail(task_info_output), _tail(frame_variable_output))
